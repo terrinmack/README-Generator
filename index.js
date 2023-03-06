7bde5c6a392f956a5ea24c6a2c45a4bc53d3490f
Your Name <you@example.com>
2023-03-06 13:16:05 -0800

    having difficulties with readme file formatting

--- index.js
+++ index.js
@@ -1,65 +1,68 @@
+
 const inquirer = require('inquirer');
-const fs = require('fs');
+const fs = require("fs");
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title for your project?',
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Enter the information you would like to include in your README description section.',
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Enter installation instructions for you project.',
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Enter the usage information for your project.',
-        },
-        {
-            type: 'input',
-            name: 'contribution',
-            message: 'Enter your project contribution guidelines.',
-        },
-        {
-            type: 'input',
-            name: 'credits',
-            message: 'What is your project title?',
-        },
-        {
-            type: 'input',
-            name: 'test',
-            message: 'What is your project title?',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'What is your project title?',
-            choices: ['MIT', 'ISC', 'None'],
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'What is your project title?',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your project title?',
-        },
-    ])
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title for your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter the information you would like to include in your README description section.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions for you project.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter the usage information for your project.',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter your project contribution guidelines.',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Is there anyone you would like to add to your credits?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What are your test instructions?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What is your project title?',
+        choices: ['MIT', 'ISC', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },  
+])
 
+.then((data) => {
 
+    fs.writeFile("SampleREADME.md", generateREADME(data))
+});
 
-
-
+function generateREADME(data) {
+    return `
 
     # ${data.title}
     ![License Badge](https://img.shields.io/github/license/${data.github}/${data.repo})
@@ -84,5 +87,7 @@ inquirer
     ## Contacts
 
     - My [Github Profile](https://github.com/${data.github})
-    - Have more questions for me? Contact me at [my email](mailto:${data.email}).
-
+    - Have more questions or feedback? Contact me at [my email](mailto:${data.email})
+    
+    `
+}
